c03a1a47ec15f8a8784c9159986ba88ff10285ff
Rita Calixto <113695803+Kassia27@users.noreply.github.com>
2024-01-17 18:48:54 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,16 @@
+"""  Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre 
+A e B é mostre se a soma é menor que C. """
+
+A = 20
+B = 13
+C = 54
+
+soma = A + B
+resultado = soma < C 
+
+print(f"{A} + {B} = {soma}")
+
+if soma < C:
+    print(f"{soma} e menor que {C}: {resultado} ")
+else:
+    print(f"{soma} e maio que {C}: {resultado}")
