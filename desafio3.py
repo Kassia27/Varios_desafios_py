191b9f9534df3f6736fb85c6fe685925347afb92
Rita Calixto <113695803+Kassia27@users.noreply.github.com>
2024-01-18 17:00:32 -0300

    Add files via upload

---
+++ desafio3.py
@@ -0,0 +1,15 @@
+""" Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores, 
+
+caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e
+
+imprimir seu valor na tela. """
+
+A = 10
+B = 20
+
+if A == B:
+    soma = int(A + B)
+    C = print(soma)
+elif A!= B:
+    multiplicao = int(A * B)
+    C = print(multiplicao)
---
+++ desafio4.py
@@ -0,0 +1,11 @@
+# Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
+
+import random
+
+numeros_aleatorios = random.randint(1,100)
+
+numero = (numeros_aleatorios)
+
+print(f"O valor antecessor de {numero} é: {numero -1}")
+print(f"O valor sucessor de {numero} é: {numero +1}")
+
